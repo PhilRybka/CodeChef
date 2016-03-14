13344b28d131c23f5d8dfd14610d0f5bdb28c8cd
Phil Rybka <filrybka@gmail.com>
2016-03-14 13:36:03 +0100

    Create Find-Remainder.cpp

---
+++ Practice-Beginner/Find-Remainder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,a,b,c;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        c=a%b;
+        cout<<c<<endl;
+    }
+}

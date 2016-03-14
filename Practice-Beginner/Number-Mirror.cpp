d7141c9cd734e1522c3bf88975aafa286956f879
Phil Rybka <filrybka@gmail.com>
2016-03-14 23:15:35 +0100

    Create Number-Mirror.cpp

---
+++ Practice-Beginner/Number-Mirror.cpp
@@ -0,0 +1,11 @@
+
+//That's probably the most sophisticated problem you'll come across on codechef.com XD
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<n;
+}
